31a951a62fd407d2927ab29b7d03a406156de675
Oguz Gur <oguz.gur.cs@gmail.com>
2024-08-27 21:44:50 +0300

    back-end and usability is nearly finished

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 API_KEY = '07a8f04e9dfc8c54484974e8b709275e'
-CITY = 'Eskişehir'
 
 app = Flask(__name__)
 
@@ -11,22 +10,50 @@ def home():
     return render_template('index.html')
 
 @app.route('/lat-lon-data', methods=['GET'])
-def get_lat_lon(city):
+def get_lat_lon_data():
+    city = request.args.get('city') 
     url = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=5&appid={API_KEY}'
     response = requests.get(url)
-    return response.json()
-
+    lat_lon_data = response.json()
+    if lat_lon_data:
+        return jsonify(lat_lon_data[0])  
+    else:
+        return jsonify({"error": "City not found"}), 404
+    
+@app.route('/city-data', methods=['GET'])
+def get_city_data():
+    lat = request.args.get('lat') 
+    lon = request.args.get('lon') 
+    url = f'http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit=5&appid={API_KEY}'
+    response = requests.get(url)
+    city_data = response.json()
+    if city_data:
+        return jsonify(city_data[0])  
+    else:
+        return jsonify({"error": "Lat and Lon are not found"}), 404
+    
+@app.route('/air-quality', methods=['GET'])
+def air_quality_data():
+    lat = request.args.get('lat') 
+    lon = request.args.get('lon') 
+    url = f'https://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={API_KEY}'
+    response = requests.get(url)
+    air_quality_data = response.json()
+    if air_quality_data:
+        return jsonify(air_quality_data)  
+    else:
+        return jsonify({"error": "Lat and Lon are not found"}), 404
 
 @app.route('/weather-data', methods=['GET'])
-def get_weather_data(lat, lon):
-    url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API_KEY}&units=metric'
-    response = requests.get(url)
-    return response.json()
+def get_weather_data():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    if lat and lon:
+        url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API_KEY}&exclude=hourly,daily,minutely&units=metric'
+        response = requests.get(url)
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Latitude and longitude are required"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-lat_lon_data = get_lat_lon(CITY)
-LAT = lat_lon_data[0]['lat']
-LON = lat_lon_data[0]['lon']
-weather_data = get_weather_data(LAT, LON)
--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,234 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    const getUserLocation = () => {
+        return new Promise((resolve, reject) => {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    position => {
+                        resolve({
+                            lat: position.coords.latitude,
+                            lon: position.coords.longitude
+                        })
+                    },
+                    (error) => {
+                        reject(error);
+                    }
+                );
+            } else {
+                reject(new Error("Geolocation is not supported by this browser."))
+            }
+        })
+    }
+
+    async function startWebpage() {
+        try {
+            const userLocation = await getUserLocation();
+            const { lat, lon } = userLocation;
+    
+            const weatherResponse = await fetch(`weather-data?lat=${lat}&lon=${lon}`);
+            const weatherData = await weatherResponse.json();
+
+            const locationResponse = await fetch(`city-data?lat=${lat}&lon=${lon}`);
+            const locationData = await locationResponse.json();
+
+            const airQualityResponse = await fetch(`air-quality?lat=${lat}&lon=${lon}`);
+            const airQualityData = await airQualityResponse.json();
+
+            new DayWeather(weatherData, locationData, airQualityData);
+
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            alert('Unable to retrieve your location.');
+        }
+    }
+
+    startWebpage();
+
+    class DayWeather {
+        constructor(weatherData, locationData, airQualityData) {
+            this.weatherData = weatherData;
+            this.locationData = locationData;
+            this.airQualityData = airQualityData;
+            console.log(airQualityData)
+
+            this.sidebarContent = document.querySelector(".sidebar-content");
+            this.temp = this.sidebarContent.querySelector("#temp");
+            this.day = this.sidebarContent.querySelector("#day");
+            this.generalInfo = this.sidebarContent.querySelector("#general-info");
+            this.rainInfo = this.sidebarContent.querySelector("#rain-info");
+            this.location = this.sidebarContent.querySelector("#location").querySelector("h2");
+
+            this.mainContent = document.querySelector(".highlights-container");
+            this.uvIndex = this.mainContent.querySelector("progress");
+            this.uvIndexInfo = this.mainContent.querySelector(".uv-index-container").querySelector("p");
+            this.windSpeed = this.mainContent.querySelector("#wind-speed");
+            this.windDirection = this.mainContent.querySelector("#wind-direction");
+            this.sunrise = this.mainContent.querySelector("#sunrise");
+            this.sunset = this.mainContent.querySelector("#sunset");
+            this.humidityPercentage = this.mainContent.querySelector("#humidity-percentage");
+            this.humidityInfo = this.mainContent.querySelector("#humidity-info");
+            this.visibilityDistance = this.mainContent.querySelector("#visibility-distance");
+            this.visibilityInfo = this.mainContent.querySelector("#visibility-info");
+            this.airQualityUnit = this.mainContent.querySelector("#air-quality-unit");
+            this.airQualityInfo = this.mainContent.querySelector("#air-quality-info");
+            
+            this.appendSidebarData();
+            this.appendMainData();
+        }
+        
+        appendSidebarData() {
+            this.temp.textContent = Math.floor(this.weatherData.current.temp);
+            const dayAndTime = this.getDayAndTime(this.weatherData.current.dt);
+            this.day.textContent = `${dayAndTime.day}, `;
+            this.day.setAttribute("data-after", dayAndTime.formattedTime);
+            this.generalInfo.textContent = this.capitalizeFirstLetter(this.weatherData.current.weather[0].description);
+            this.location.textContent = this.locationData.name;
+            
+            this.rainInfo.setAttribute(
+                "data-after", 
+                this.weatherData.current.rain ? `${this.weatherData.current.rain['1h'] * 100}%` : `0%`
+                );            
+        }
+            
+        appendMainData() {
+            this.uvIndex.value = this.weatherData.current.uvi;
+            this.uvIndexInfo.textContent = this.getUvInfo(this.weatherData.current.uvi);
+            this.windSpeed.textContent = `${this.weatherData.current.wind_speed}km/h`;
+            this.windDirection.textContent = this.getWindDirection(this.weatherData.current.wind_deg);
+            this.sunrise.textContent = `${this.getDayAndTime(this.weatherData.current.sunrise).formattedTime}`;
+            this.sunset.textContent = `${this.getDayAndTime(this.weatherData.current.sunset).formattedTime}`;
+            this.humidityPercentage.textContent = `${this.weatherData.current.humidity}%`;
+            this.humidityInfo.textContent = this.getHumidityInfo(this.weatherData.current.humidity);
+            this.visibilityDistance.textContent = `${this.weatherData.current.visibility / 1000}km`;
+            this.visibilityInfo.textContent = `${this.getVisibilityInfo(this.weatherData.current.visibility)}`;
+            this.airQualityUnit.textContent = this.airQualityData.list[0].main.aqi;
+            this.airQualityInfo.textContent = `${this.getAirQualityInfo(this.airQualityData.list[0].main.aqi)}`;
+        }
+            
+        getDayAndTime(timestamp) {  
+            let date = new Date(timestamp * 1000);
+            // Get day of the week
+            let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            let day = daysOfWeek[date.getDay()];
+
+            // Get hours and minutes
+            let hours = date.getHours();
+            let minutes = date.getMinutes();
+
+            // Format hours and minutes (e.g., 12.50)
+            let formattedTime = `${hours}.${minutes < 10 ? '0' + minutes : minutes}`;
+            return {day, formattedTime};
+        }
+
+        capitalizeFirstLetter (str) {
+            return str.split(' ')                  // Split the string into an array of words
+                .map(word => 
+                    word.charAt(0).toUpperCase() + // Capitalize the first letter of each word
+                    word.slice(1).toLowerCase()    // Convert the rest of the word to lowercase
+                )
+                .join(' ');                        // Join the array back into a single string
+        }
+
+        getUvInfo(index) {
+            if (index <= 3) {
+                return "Low";
+            } else if (index <= 6) {
+                return "Moderate";
+            } else if (index <= 9) {
+                return "High";
+            } else if (index <= 12) {
+                return "Very High";
+            } else {
+                return "Extreme";
+            }
+        }
+
+        getWindDirection(degree) {
+            if (degree >= 348.75 || degree < 11.25) {
+                return 'N';    // North
+            } else if (degree >= 11.25 && degree < 33.75) {
+                return 'NNE';  // North-Northeast
+            } else if (degree >= 33.75 && degree < 56.25) {
+                return 'NE';   // Northeast
+            } else if (degree >= 56.25 && degree < 78.75) {
+                return 'ENE';  // East-Northeast
+            } else if (degree >= 78.75 && degree < 101.25) {
+                return 'E';    // East
+            } else if (degree >= 101.25 && degree < 123.75) {
+                return 'ESE';  // East-Southeast
+            } else if (degree >= 123.75 && degree < 146.25) {
+                return 'SE';   // Southeast
+            } else if (degree >= 146.25 && degree < 168.75) {
+                return 'SSE';  // South-Southeast
+            } else if (degree >= 168.75 && degree < 191.25) {
+                return 'S';    // South
+            } else if (degree >= 191.25 && degree < 213.75) {
+                return 'SSW';  // South-Southwest
+            } else if (degree >= 213.75 && degree < 236.25) {
+                return 'SW';   // Southwest
+            } else if (degree >= 236.25 && degree < 258.75) {
+                return 'WSW';  // West-Southwest
+            } else if (degree >= 258.75 && degree < 281.25) {
+                return 'W';    // West
+            } else if (degree >= 281.25 && degree < 303.75) {
+                return 'WNW';  // West-Northwest
+            } else if (degree >= 303.75 && degree < 326.25) {
+                return 'NW';   // Northwest
+            } else if (degree >= 326.25 && degree < 348.75) {
+                return 'NNW';  // North-Northwest
+            }
+        }
+        
+        getHumidityInfo(percentage) {
+            if (percentage <= 30) {
+                return "Low";
+            } else if (percentage <= 60) {
+                return "Moderate";
+            } else if (percentage <= 80) {
+                return "High";
+            } else {
+                return "Very High";
+            }
+        }
+        
+        getVisibilityInfo(meters) {
+            if (meters <= 1000) {
+                return "Very Low";
+            } else if (meters <= 5000) {
+                return "Low";
+            } else if (meters <= 10000) {
+                return "Moderate";
+            } else {
+                return "High";
+            }
+        }  
+        
+        getAirQualityInfo(aqi) {
+            if (aqi === 1) {
+                return "Good";
+            } else if (aqi === 2) {
+                return "Fair";
+            } else if (aqi === 3) {
+                return "Moderate";
+            } else if (aqi === 4) {
+                return "Poor";
+            } else if (aqi === 5) {
+                return "Very Poor";
+            } else {
+                return "Data not available.";
+            }
+        }
+
+    }
+        
+    class WeekWeather {
+        constructor () {
+            this.template = document.querySelector("#day-template");
+        }
+    }
+    
+    const weekWeather = new WeekWeather();
+
     const units = document.querySelectorAll(".unit");
     const unitSelection = document.querySelector("#unit-selection");
     
@@ -33,39 +263,4 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     });
-
-    class DayWeather {
-        constructor() {
-            this.sidebarContent = document.querySelector(".sidebar-content");
-            this.temp = this.sidebarContent.document.querySelector("#temp");
-            this.day = this.sidebarContent.document.querySelector("#day");
-            this.generalInfo = this.sidebarContent.document.querySelector("#general-info");
-            this.rainInfo = this.sidebarContent.document.querySelector("#rain-info");
-            this.location = this.sidebarContent.document.querySelector("#location");
-
-            this.mainContent = document.querySelector(".highlights-container");
-            this.uvIndex = this.mainContent.document.querySelector("progress");
-            this.windSpeed = this.mainContent.document.querySelector("#wind-speed");
-            this.windDirection = this.mainContent.document.querySelector("#wind-direction");
-            this.sunrise = this.mainContent.document.querySelector("#sunrise");
-            this.sunset = this.mainContent.document.querySelector("#sunset");
-            this.humidityPercentage = this.mainContent.document.querySelector("#humidity-percentage");
-            this.humidityInfo = this.mainContent.document.querySelector("#humidity-info");
-            this.visibilityPercentage = this.mainContent.document.querySelector("#visibility-percentage");
-            this.visibilityInfo = this.mainContent.document.querySelector("#visibility-info");
-            this.airQualityPercentage = this.mainContent.document.querySelector("#air-quality-percentage");
-            this.airQualityInfo = this.mainContent.document.querySelector("#air-quality-info");
-        }
-
-
-    }
-    
-    class WeekWeather {
-        constructor () {
-            this.template = document.querySelector("#day-template");
-        }
-    }
-    
-    const dayWeather = new DayWeather();
-    const weekWeather = new WeekWeather();
 });
